6f0ea3107f0602a1bc24f8e8155966e3ed8ae531
Gabriella Uwadiegwu <gabriella.uwadiegwu@gpshopper.com>
2018-11-14 21:15:39 -0500

    New stuff

--- scripts2.js
+++ scripts2.js
@@ -1,7 +1,7 @@
 var a = [];
 var ipVal = [];
 function classA() {
-    for (let ip of range('0.0.0.0', '0.0.0.20')) {
+    for (let ip of range('0.0.0.0', '0.0.0.60')) {
         a.push(ip);
     }
     return a;
@@ -37,68 +37,77 @@ function classE() {
 
 function start(){
 
-        var ipType = document.getElementById("ip4").value;
-        if (ipType === undefined)
-            ipType = document.getElementById("ip6").value;
+    var ipType = document.getElementById("ip4").value;
+    if (ipType === undefined)
+        ipType = document.getElementById("ip6").value;
 
-        var cls = document.getElementById("ip4type").value;
-        if (cls === undefined)
-            cls = document.getElementById("ip6type").value;
+    var cls = document.getElementById("ip4type").value;
+    if (cls === undefined)
+        cls = document.getElementById("ip6type").value;
 
-        var n = document.getElementById("n").value;
+    var n = document.getElementById("n").value;
 
-        var i;
-        if(ipType === "IPV4"){
-            if(cls === "Classful"){
-                var dropDn = document.getElementById("Classful").value;
-                if(dropDn === "Class A"){
-                    classA();
-                    for(i = 0; i<n; i++)
-                        ipVal.push(a[Math.floor(Math.random() * a.length)]);
-                    store(ipVal);
-                }
-                else if(dropDn === "Class B"){
-                    classB();
-                    for(i = 0; i<n; i++)
-                        ipVal.push(a[Math.floor(Math.random() * a.length)]);
-                    store(ipVal);
-                }
-                else if(dropDn === "Class C"){
-                    classC();
-                    for(i = 0; i<n; i++)
-                        ipVal.push(a[Math.floor(Math.random() * a.length)]);
-                    store(ipVal);
-                }
-                else if(dropDn === "Class D"){
-                    classD();
-                    for(i = 0; i<n; i++)
-                        ipVal.push(a[Math.floor(Math.random() * a.length)]);
-                    store(ipVal);
-                }
-                else{
-                    classE();
-                    for(i = 0; i<n; i++)
-                        ipVal.push(a[Math.floor(Math.random() * a.length)]);
-                    store(ipVal);
-                }
-                store(ipVal)
+    var i;
+    if(ipType === "IPV4"){
+        if(cls === "Classful"){
+            var dropDn = document.getElementById("Classful").value;
+            if(dropDn === "Class A"){
+                classA();
+                for(i = 0; i<n; i++)
+                    ipVal.push(a[Math.floor(Math.random() * a.length)]);
+                store(ipVal,n);
+            }
+            else if(dropDn === "Class B"){
+                classB();
+                for(i = 0; i<n; i++)
+                    ipVal.push(a[Math.floor(Math.random() * a.length)]);
+                store(ipVal,n);
+            }
+            else if(dropDn === "Class C"){
+                classC();
+                for(i = 0; i<n; i++)
+                    ipVal.push(a[Math.floor(Math.random() * a.length)]);
+                store(ipVal,n);
+            }
+            else if(dropDn === "Class D"){
+                classD();
+                for(i = 0; i<n; i++)
+                    ipVal.push(a[Math.floor(Math.random() * a.length)]);
+                store(ipVal,n);
             }
             else{
-                //classless
+                classE();
+                for(i = 0; i<n; i++)
+                    ipVal.push(a[Math.floor(Math.random() * a.length)]);
+                store(ipVal,n);
             }
         }
+        else{
+            //classless
+        }
+    }
 }
 
-function store(ips){
+function store(ips, n){
     var i;
     for(i = 0; i < ips.length; i++){
         if(localStorage.getItem(ips[i]))
             console.log("nothing");
         else
+            var ip = "";
+            var j;
+            for(j = 0; j<n; j++)
+                ip += ips[j] + "\n";
             localStorage.setItem(ips[i], ips[i]);
-            document.getElementById("output").innerText = ips[i];
+            document.getElementById("output").innerText = ip;
 
     }
+}
+
+//Zero compression
+function reduce(ip) {
+    let ip6 = require('ip6');
+    return abbreviate(ip);
 }
 
 'use strict';
@@ -155,4 +164,59 @@ function *range(ip1, ip2) {
         yield `${(i >> 24) & 0xff}.${(i >> 16) & 0xff}.${(i >> 8) & 0xff}.${i & 0xff}`;
     }
 }
+
+//Sourced form ip6
+let abbreviate = function (a) {
+    if (!_validate(a)) {
+        throw new Error('Invalid address: ' + a);
+    }
+    a = normalize(a);
+    a = a.replace(/0000/g, 'g');
+    a = a.replace(/\:000/g, ':');
+    a = a.replace(/\:00/g, ':');
+    a = a.replace(/\:0/g, ':');
+    a = a.replace(/g/g, '0');
+    let sections = a.split(/\:/g);
+    let zPreviousFlag = false;
+    let zeroStartIndex = -1;
+    let zeroLength = 0;
+    let zStartIndex = -1;
+    let zLength = 0;
+    for (let i = 0; i < 8; ++i) {
+        let section = sections[i];
+        let zFlag = (section === '0');
+        if (zFlag && !zPreviousFlag) {
+            zStartIndex = i;
+        }
+        if (!zFlag && zPreviousFlag) {
+            zLength = i - zStartIndex;
+        }
+        if (zLength > 1 && zLength > zeroLength) {
+            zeroStartIndex = zStartIndex;
+            zeroLength = zLength;
+        }
+        zPreviousFlag = (section === '0');
+    }
+    if (zPreviousFlag) {
+        zLength = 8 - zStartIndex;
+    }
+    if (zLength > 1 && zLength > zeroLength) {
+        zeroStartIndex = zStartIndex;
+        zeroLength = zLength;
+    }
+    //console.log(zeroStartIndex, zeroLength);
+    //console.log(sections);
+    if (zeroStartIndex >= 0 && zeroLength > 1) {
+        sections.splice(zeroStartIndex, zeroLength, 'g');
+    }
+    //console.log(sections);
+    a = sections.join(':');
+    //console.log(a);
+    a = a.replace(/\:g\:/g, '::');
+    a = a.replace(/\:g/g, '::');
+    a = a.replace(/g\:/g, '::');
+    a = a.replace(/g/g, '::');
+    //console.log(a);
+    return a;
+};
 
